559c65243e7852c39aeb0b40c0119bb123a1ae16
emilMircea <emil.mircea30@gmail.com>
2017-07-08 13:53:58 +0200

    added sign up form on the sign up page

--- src/pages/Users/New.js
+++ src/pages/Users/New.js
@@ -3,29 +3,28 @@ import Page from '../../components/Page'
 import { Link } from 'react-router'
 
 class New extends React.PureComponent {
-  // extras = 'Already have an account? Sign In'
+
+  submitForm = (e) =>
+    e.preventDefault()
+
   render() {
     const extras = <Link to='users/sign_in'>Already have an account? Sign In here.</Link>
     return (
       <Page.Auth title='Sign Up Page' extras={extras}>
         <form className='user-registration-form animated fadeIn' onSubmit={this.submitForm}>
           <div className="form-group">
-            <label htmlFor="nameInput">Email</label>
-            <input ref={(input) => this.name = input} type="name" className="form-control" id="nameInput" placeholder="name" />
-          </div>
-          <div className="form-group">
             <label htmlFor="emailInput">Email</label>
             <input ref={(input) => this.email = input} type="email" className="form-control" id="emailInput" placeholder="email" />
           </div>
           <div className="form-group">
             <label htmlFor="passwordInput">Password</label>
-            <input ref={(input) => this.password = input} type="password" className="form-control" id="passwordInput" placeholder="password" />
+            <input ref={(input) => this.password = input} type="password" className="form-control" id="passwordInput"  />
           </div>
           <div className="form-group">
             <label htmlFor="passwordInput">Repeat Password</label>
-            <input ref={(input) => this.password = input} type="password" className="form-control" id="passwordInput" placeholder="repeat password" />
+            <input ref={(input) => this.password_confirm = input} type="password" className="form-control" id="passwordInput" />
           </div>
-          <button className="btn btn-primary logInBtn" type='submit'>Log In</button>
+          <button className="btn btn-primary logInBtn" type='submit'>Sign Up</button>
         </form>
       </Page.Auth>
     )
